1c54e3b883386cf8a5a9d7bca558c10f7fe40df4
valentina lopez <140413463+valenlopezzz04@users.noreply.github.com>
2024-08-09 14:08:23 -0500

    Update orderDessert.java

--- src/Patron_3/orderDessert.java
+++ src/Patron_3/orderDessert.java
@@ -14,14 +14,14 @@ public class orderDessert extends Orden {
 
     @Override
     protected boolean verificarDisponibilidad() {
-        // Aquí se podría consultar un inventario, por simplicidad se devuelve true
+       
         System.out.println("Verificando disponibilidad de postre...");
         return true;
     }
 
     @Override
     protected int calcularPrecio() {
-        // Aquí se calcularía el precio del postre, por simplicidad se devuelve un valor fijo
+      
         System.out.println("Calculando precio de postre...");
         return 7;
     }
